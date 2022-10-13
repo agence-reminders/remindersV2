eca428d6ebc46bce2c7f8e98cec0a08a5041406f
moshi <sporever>
2022-10-13 03:33:45 +0200

    Parallax

--- js/index.js
+++ js/index.js
@@ -5,3 +5,36 @@ $links.forEach((item) => {
     const text = item.innerText;
     item.innerHTML += `<span data-letters="${text}"></span><span data-letters="${text}"></span>`
 });
+
+
+/*** parallax **/
+
+// The parallax function
+const parallax = () => {
+    const elements = document.querySelectorAll('.parallax')
+    if ('undefined' !== elements && elements.length > 0) {
+        elements.forEach( element => {
+            let y = window.innerHeight - element.getBoundingClientRect().top
+            if (y > 0) {
+                element.style.transform = 'translate3d(0, -' + (0.1 * y) + 'px ,0)'
+            }
+        })
+    }
+}
+
+parallax();
+
+window.addEventListener('scroll', throttle(parallax, 14));
+
+// pour optimiser les performances
+function throttle(fn, wait) {
+    let time = Date.now();
+    return function() {
+        if ((time + wait - Date.now()) < 0) {
+            fn();
+            time = Date.now();
+        }
+    }
+};
+
+
