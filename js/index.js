ecdd19c3c78061fdc0797eceacdc95f512449e12
moshi <sporever>
2022-10-13 06:12:15 +0200

    Animations

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 /*** link--rd **/
 const $links = document.querySelectorAll('.link--rd');
 
@@ -6,25 +7,35 @@ $links.forEach((item) => {
     item.innerHTML += `<span data-letters="${text}"></span><span data-letters="${text}"></span>`
 });
 
+/** menu highlight **/
+    $links.forEach((item, nb) => {
+        setTimeout(() => {
+            item.classList.add('hover')
+            setTimeout(() => {
+                item.classList.remove('hover')
+            }, 800)
+        }, 3000 + (nb * 1000));
+    })
 
 /*** parallax **/
 
 // The parallax function
 const parallax = () => {
-    const elements = document.querySelectorAll('.parallax')
-    if ('undefined' !== elements && elements.length > 0) {
-        elements.forEach( element => {
-            let y = window.innerHeight - element.getBoundingClientRect().top
-            if (y > 0) {
-                element.style.transform = 'translate3d(0, -' + (0.1 * y) + 'px ,0)'
-            }
-        })
+    const elementsFooter = document.querySelector('.footer--wrapper')
+    if ('undefined' !== elementsFooter) {
+        let y = elementsFooter.offsetTop + elementsFooter.scrollHeight - window.scrollY - window.innerHeight
+        if (1) {
+            /*
+            TweenMax.to(elementsFooter, 0, {
+                transform: 'translate3d(0, -' + (0.2 * y) + 'px ,0)'
+            })*/
+            elementsFooter.style.transform = 'translate3d(0, -' + (0.2 * y) + 'px ,0)'
+        }
+
     }
 }
 
-parallax();
-
-window.addEventListener('scroll', throttle(parallax, 14));
+window.addEventListener('scroll', throttle(parallax, 1));
 
 // pour optimiser les performances
 function throttle(fn, wait) {
@@ -36,5 +47,59 @@ function throttle(fn, wait) {
         }
     }
 };
+
+/*** effect appears **/
+AOS.init();
+
+/** textwrapper **/
+var textWrapper = document.querySelector('.appears--letter-1');
+textWrapper.style.opacity="0";
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+setTimeout(() => {
+    textWrapper.style.opacity="1";
+    anime.timeline({loop: true})
+        .add({
+            targets: '.line-wrapper .line',
+            scaleY: [0,1],
+            opacity: [0.5,1],
+            easing: "easeOutExpo",
+            duration: 700
+        })
+        .add({
+            targets: '.line-wrapper .line',
+            translateX: [0, document.querySelector('.appears--letter-1').getBoundingClientRect().width + 10],
+            easing: "easeOutExpo",
+            duration: 700,
+            delay: 100
+        }).add({
+        targets: '.line-wrapper .letter',
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=775',
+        delay: (el, i) => 34 * (i+1)
+    }).add({
+        targets: '.line-wrapper',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+}, 2000)
+
+
+/*** GSAP **/
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const smoother = ScrollSmoother.create({
+    wrapper: "#wrapper",
+    content: "#content",
+    smooth: 0.8,
+    normalizeScroll: true, // prevents address bar from showing/hiding on most devices, solves various other browser inconsistencies
+    ignoreMobileResize: true, // skips ScrollTrigger.refresh() on mobile resizes from address bar showing/hiding
+    effects: true,
+    preventDefault: true
+});
 
 
